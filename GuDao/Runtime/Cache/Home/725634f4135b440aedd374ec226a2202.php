9e28c788baa8ee5bbdf7f73d53e4e026460ef178
RainieWu <479086484@qq.com>
2018-02-11 18:07:32 +0800

    导航条&页脚&返回顶部&弹窗&查看原图&检查登录状态&退出登录

--- GuDao/Runtime/Cache/Home/725634f4135b440aedd374ec226a2202.php
+++ GuDao/Runtime/Cache/Home/725634f4135b440aedd374ec226a2202.php
@@ -2,134 +2,125 @@
 <html>
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <title></title>
 <link rel="stylesheet" type="text/css" href="/GuDao/Public/css/common/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="/GuDao/Public/css/common/common.css">
-<script src="/GuDao/Public/js/common/jquery-3.2.1.js" type="text/javascript" charset="utf-8"></script>
-<script src="/GuDao/Public/js/common/bootstrap.js" type="text/javascript" charset="utf-8"></script>
+<link rel="stylesheet" type="text/css" href="/GuDao/Public/css/common/frame.css">
+<script type="text/javascript" src="/GuDao/Public/js/common/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="/GuDao/Public/js/common/bootstrap.js"></script>
 </head>
-<body>
-	<nav class="navbar navbar-fixed-top">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-3 logo">孤岛</div>
-				<div class="col-sm-1 menu active"><a href="">首页</a></div>
-				<div class="col-sm-1 menu"><a href="../Notice">通知</a></div>
-				<div class="col-sm-1 menu"><a href="../Show">演出</a></div>
-				<div class="col-sm-1 menu"><a href="../Band">乐队</a></div>
-				<div class="col-sm-3 search">
-					<form action="">
-						<input type="text" name="search" placeholder="搜索...">
-						<button><span class="glyphicon glyphicon-search"></span></button>
-					</form>
-				</div>
-				<div class="col-sm-2 didnotLogin"><a href="login">登录</a>&ensp;|&ensp;<a href="register">注册</a></div>
-				<div class="col-sm-2 didLogin">
-					<img src="/GuDao/Public/img/user/headImg.jpg">
-					<div class="userList">
-						<ul>
-							<li class="triangle">
-								<span class="triangleBorder"></span>
-								<span class="triangleColor"></span>
-							</li>
-							<li class="username">NgWingLam</li>
-							<li><a href="../User">个人中心<span></span></a></li>
-							<li><a href="../User">我的消息<span class="spot"></span></a></li>
-							<li><a href="">退出登录<span></span></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
+<body id="top">
+<!-- 导航条 -->
+<nav class="navbar navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<!-- 展开菜单 -->
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<!-- Logo -->
+			<a class="navbar-brand" href=""><img src="/GuDao/Public/img/common/nav_logo.png"></a>
 		</div>
-	</nav>
-
-	<div class="container" id="home">
-			<p>start</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>a</p>
-			<p>end</p>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<!-- 未登录 -->
+			<p class="didnotLogin navbar-text navbar-right"><a href="login" class="navbar-link">登录</a>&ensp;|&ensp;<a href="register" class="navbar-link">注册</a></p>
+			<!-- 已登录 -->
+			<ul class="didLogin nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="/GuDao/Public/img/user/headImg.jpg"></a>
+					<ul class="dropdown-menu">
+						<li class="dropdown-header">NgWingLam</li>
+						<li role="separator" class="divider"></li>
+						<li><a href="../User">个人中心</a></li>
+						<li><a href="../User" class="">我的消息<span class="dot-span"></span></a></li>
+						<li class="logout"><a href="#">退出登录</a></li>
+					</ul>
+				</li>
+			</ul>
+			<!-- 表单 -->
+			<form class="navbar-form navbar-right">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="搜索...">
+					<span class="input-group-btn">
+						<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+					</span>
+				</div>
+			</form>
+			<!-- 菜单 -->
+			<ul class="nav navbar-nav navbar-right">
+				<li class="active"><a href="">首页</a></li>
+				<li><a href="../Notice">通知</a></li>
+				<li><a href="../Show">演出</a></li>
+				<li><a href="../Band">乐队</a></li>
+			</ul>
+		</div>
 	</div>
+</nav>
 
-	<a href="#home" class="backToTop"><span class="glyphicon glyphicon-send"></span></a>
-	<footer>Copyright&ensp;&copy;&ensp;2017-2018&ensp;孤岛音乐平台&ensp;All&ensp;Rights&ensp;Reserved.</footer>
+<!-- 内容 -->
+<div class="container content" id="home">
+	<p>start</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>a</p>
+	<p>end</p>
+</div>
+
+<!-- 底部 -->
+<a class="back-to-top"><span class="glyphicon glyphicon-send"></span></a>
+<footer>Copyright&ensp;&copy;&ensp;2017-2018&ensp;孤岛音乐平台&ensp;All&ensp;Rights&ensp;Reserved.</footer>
 
 
-	
-
+<script type="text/javascript" src="/GuDao/Public/js/common/common.js"></script>
+<script type="text/javascript" src="/GuDao/Public/js/common/frame.js"></script>
 <script>
 	// $.ajax({
 	// 	url: "/GuDao/index.php/Home/Index/getShowCalendar",
@@ -142,45 +133,6 @@
 	// 		console.log(result);
 	// 	}
 	// });
-
-
-	$(function() {
-		//sessionStorage.setItem("userID", 1);
-		if(sessionStorage.getItem("userID")) {
-			$(".didnotLogin").hide();
-		} else {
-			$(".didLogin").hide();
-		}
-
-		$(".didLogin img").mouseover(function() {
-			$(".didLogin .userList").show();
-		});
-		$(".didLogin img").click(function(e) {
-			$(".didLogin .userList").show();
-			$(document).one("click", function() {
-				$(".didLogin .userList").hide();
-			});
-			e.stopPropagation();
-		});
-		$(".didLogin .userList").mouseover(function() {
-			$(".didLogin .userList").show();
-		});
-		$(".didLogin .userList").mouseout(function() {
-			$(".didLogin .userList").hide();
-		});
-		$(".didLogin .userList").click(function(e) {
-			e.stopPropagation();
-		});
-
-		$(document).scroll(function() {
-			if($(document).scrollTop() > 150) {
-				$(".backToTop").removeClass("hideBackToTop").addClass("showBackToTop");
-			} else if($(".backToTop").hasClass("showBackToTop")) {
-				$(".backToTop").removeClass("showBackToTop").addClass("hideBackToTop");
-			}
-		});
-	});
-	
 </script>
 </body>
 </html>
---
+++ Public/js/common/frame.js
@@ -0,0 +1,58 @@
+$(function() {
+	// 检查登录状态
+	// sessionStorage.setItem("userID", 1);
+	if(sessionStorage.getItem("userID")) {
+		$(".didnotLogin").hide();
+	} else {
+		$(".didLogin").hide();
+	}
+
+
+	// 退出登录
+	$(".logout").click(function(e) {
+		e.preventDefault();
+		setAlertBox({
+			className: "text",
+			close: true,
+			title: "孤岛提示",
+			message: "您确定要退出登录吗？",
+			buttons: [{
+				value: "确定",
+				callback: function() {
+					sessionStorage.removeItem("userID");
+					location.reload();
+				}
+			}, {
+				value: "取消"
+			}]
+		});
+	});
+
+
+	// 返回顶部
+	$(document).scroll(function() {
+		if($(document).scrollTop() > 150) {
+			$(".back-to-top").removeClass("hide-back-to-top").addClass("show-back-to-top");
+		} else if($(".back-to-top").hasClass("show-back-to-top")) {
+			$(".back-to-top").removeClass("show-back-to-top").addClass("hide-back-to-top");
+		}
+	});
+	$(".back-to-top").click(function() {
+		$(document).scrollTop(0);
+	});
+
+
+	// 查看原图
+	$("img.thumbnail").click(function() {
+		setAlertBox({
+			className: "image",
+			close: true,
+			maskClose: true,
+			message: "<img src=" + this.src +">",
+			buttons: []
+		});
+		if($(".alert-box.image .message").height() > $(".alert-box.image .box").height()) {
+			$(".alert-box.image .message").height($(".alert-box.image .box").height() - 72);
+		}
+	});
+});
