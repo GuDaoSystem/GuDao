50fdffcf8a44b8403ba5753dea94371c143cab24
RainieWu <479086484@qq.com>
2018-03-16 20:52:51 +0800

    通知列表额

--- GuDao/Runtime/Cache/Home/0b9422e265b26a78540e91be8d993c80.php
+++ GuDao/Runtime/Cache/Home/0b9422e265b26a78540e91be8d993c80.php
@@ -7,6 +7,7 @@
 <link rel="stylesheet" type="text/css" href="/GuDao/Public/css/common/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="/GuDao/Public/css/common/common.css">
 <link rel="stylesheet" type="text/css" href="/GuDao/Public/css/common/frame.css">
+<link rel="stylesheet" type="text/css" href="/GuDao/Public/css/notice/notice.css">
 <script type="text/javascript" src="/GuDao/Public/js/common/jquery-3.2.1.js"></script>
 <script type="text/javascript" src="/GuDao/Public/js/common/bootstrap.js"></script>
 <script type="text/javascript" src="/GuDao/Public/js/common/vue.js"></script>
@@ -19,6 +20,19 @@
 
 	<!-- 内容 -->
 	<div id="notice" class="container content">
+		<ul class="condition">
+			<li class="active"><a>全部</a></li>|<li><a>预售</a></li>|<li><a>取消</a></li>|<li><a>变更</a></li>
+		</ul>
+		<div v-show="!notices" class="no-data">
+			<img src="/GuDao/Public/img/common/noData.png">
+			<span></span>
+			<p>没有相关数据</p>
+		</div>
+		<ul class="list">
+			<li v-for="notice in notices">
+				<span v-if="notice.notice_type == '1'" class="state">预售/</span><span v-if="notice.notice_type == '2'" class="state">取消/</span><span v-if="notice.notice_type == '3'" class="state">变更/</span><p>{{notice.notice_content}}</p><span class="time">{{notice.notice_time}}</span>
+			</li>
+		</ul>
 	</div>
 
 	<!-- 底部 -->
@@ -35,33 +49,33 @@
 <script>
 $(function() {
 	// 获取全部
-	$.ajax({
-		url: "/GuDao/index.php/Home/Notice/getNoticeByPage",
-		type: "GET",
-		dataType: "json",
-		data: {
-			"pageIndex": 1,
-			"pageSize": 10
-		},
-		success: function(result) {
-			console.log(result);
-		}
-	});
+	// $.ajax({
+	// 	url: "/GuDao/index.php/Home/Notice/getNoticeByPage",
+	// 	type: "GET",
+	// 	dataType: "json",
+	// 	data: {
+	// 		"pageIndex": 1,
+	// 		"pageSize": 10
+	// 	},
+	// 	success: function(result) {
+	// 		console.log(result);
+	// 	}
+	// });
 	
-	// 按类型获取
-	$.ajax({
-		url: "/GuDao/index.php/Home/Notice/getNoticeByPage",
-		type: "GET",
-		dataType: "json",
-		data: {
-			"pageIndex": 1,
-			"pageSize": 10,
-			"type": 1
-		},
-		success: function(result) {
-			console.log(result);
-		}
-	});
+	// // 按类型获取
+	// $.ajax({
+	// 	url: "/GuDao/index.php/Home/Notice/getNoticeByPage",
+	// 	type: "GET",
+	// 	dataType: "json",
+	// 	data: {
+	// 		"pageIndex": 1,
+	// 		"pageSize": 10,
+	// 		"type": 1
+	// 	},
+	// 	success: function(result) {
+	// 		console.log(result);
+	// 	}
+	// });
 });
 </script>
 
--- Public/js/notice/notice.js
+++ Public/js/notice/notice.js
@@ -1,6 +1,7 @@
 new Vue({
 	el: '#gudao',
 	data: {
+		notices: []
 	},
 	components: {
 		"navbar": navbar,
@@ -16,6 +17,7 @@ new Vue({
 		});
 
 		// 获取数据
+		this.getNotice();
 	},
 	mounted: function() {
 		this.$nextTick(function () {
@@ -23,10 +25,64 @@ new Vue({
 	},
 	updated: function () {
 		this.$nextTick(function () {
+			// 监听筛选条件
+			$(".condition a").unbind("click").click(this.getNoticeByType);
 		});
 	},
 	computed: {
 	},
 	methods: {
+		getNotice: function() {
+			var _this = this;
+			$.ajax({
+				url: "Notice/getNoticeByPage",
+				type: "GET",
+				dataType: "json",
+				data: {
+					"pageIndex": 1,
+					"pageSize": 10
+				},
+				success: function(result) {
+					// console.log(result);
+					if(result.code === 200) {
+						var data = result.data;
+						for(var i = 0; i < data.length; i++) {
+							var time = data[i].notice_time.toString().split(" ")[0].split("-");
+							data[i].notice_time = time[0] + "年" + time[1] + "月" + time[2] + "日";
+						}
+					}
+					_this.notices = result.data;
+				}
+			});
+		},
+		getNoticeByType: function(e) {
+			var _this = this;
+			if(!$(e.target).hasClass("active")) {
+				$(".condition li").removeClass("active");
+				$(e.target).parent().addClass("active");
+
+				$.ajax({
+					url: "Notice/getNoticeByPage",
+					type: "GET",
+					dataType: "json",
+					data: {
+						"pageIndex": 1,
+						"pageSize": 10,
+						"type": $(".condition li").index($(e.target).parent())
+					},
+					success: function(result) {
+						console.log(result);
+						if(result.code === 200) {
+							var data = result.data;
+							for(var i = 0; i < data.length; i++) {
+								var time = data[i].notice_time.toString().split(" ")[0].split("-");
+								data[i].notice_time = time[0] + "年" + time[1] + "月" + time[2] + "日";
+							}
+						}
+						_this.notices = result.data;
+					}
+				});
+			}
+		}
 	}
 });
