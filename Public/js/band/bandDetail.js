8682aa2ca58b1c1d2c1e37ab7bd644d58074e5dd
RainieWu <479086484@qq.com>
2018-03-20 21:11:14 +0800

    乐队详细页 头部

--- Public/js/band/bandDetail.js
+++ Public/js/band/bandDetail.js
@@ -1,6 +1,9 @@
 new Vue({
 	el: '#gudao',
 	data: {
+		band: {},
+		supportNum: 0,
+		support: false,
 	},
 	components: {
 		"navbar": navbar,
@@ -16,9 +19,23 @@ new Vue({
 		});
 
 		// 获取数据
+		this.getBand();
 	},
 	mounted: function() {
 		this.$nextTick(function () {
+			// 标签页定位
+			var tabList = ["#show", "#picture", "#comment"];
+			var tabIndex = tabList.indexOf(location.hash);
+			$(".tablist li:eq(" + tabIndex +")").addClass("active");
+			$(".tablist .underline").addClass("tab" + (tabIndex + 1));
+			$(tabList[tabIndex]).addClass("in").addClass("active");
+
+			// 标签页切换
+			$(".tablist a").unbind("click").click(function () {
+				location.href = location.toString().split("#")[0] + $(this).attr("href");
+				$(".tablist .underline")[0].className = "underline";
+				$(".tablist .underline").addClass($(this).parent()[0].className);
+			});
 		});
 	},
 	updated: function () {
@@ -28,5 +45,53 @@ new Vue({
 	computed: {
 	},
 	methods: {
+		getBand: function() {
+			var _this = this;
+			$.ajax({
+				url: "getBandByID",
+				type: "GET",
+				dataType: "json",
+				data: {
+					"id": location.search.toString().substr(1).split("=")[1]
+				},
+				success: function(result) {
+					// console.log(result);
+					_this.band = result.data;
+					console.log(_this.band.band_intro);
+				}
+			});
+		},
+		getSupportNum: function() {
+			var _this = this;
+			$.ajax({
+				url: "__URL__/getSupportUserNum",
+				type: "GET",
+				dataType: "json",
+				data: {
+					"id": location.search.toString().substr(1).split("=")[1]
+				},
+				success: function(result) {
+					// console.log(result);
+					_this.supportNum = result.data;
+				}
+			});
+		},
+		checkSupport: function() {
+			$.ajax({
+				url: "__URL__/checkSupport",
+				type: "POST",
+				dataType: "json",
+				data: {
+					"user_id": sessionStorage.getItem("userID"),
+					"band_id": location.search.toString().substr(1).split("=")[1]
+				},
+				success: function(result) {
+					// console.log(result);
+					if(result.code === 200) {
+						_this.support = true;
+					}
+				}
+			});
+		}
 	}
 });
