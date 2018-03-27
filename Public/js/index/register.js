a30ae692897d46d091bb33b8a40561cc1758f1a7
RainieWu <479086484@qq.com>
2018-03-27 11:09:00 +0800

    整理Index/login、Index/register、User/password代码

--- Public/js/index/register.js
+++ Public/js/index/register.js
@@ -8,33 +8,6 @@ new Vue({
 });
 
 $(function() {
-	// var obj = {
-	// 		form: {							// 必选，对象，获取验证码的表单设置
-	// 			targetType: "email",				// 可选，字符串，目标类型，取值为"phone"或"email"，默认为"phone"
-	// 			inputName: "email",				// 可选，字符串，填写手机号码或邮箱地址的input标签的name属性值，默认为"phone"
-	// 			// getCodeClass: "getCode",			// 可选，字符串，获取验证码按钮的button标签的class，默认为"getCode"
-	// 			// countdown: 60,				// 可选，数值，重新获取验证码的等待时间，默认为60
-	// 			// countdownText: ""			// 可选，字符串，等待重新获取验证码时按钮的文本内容，默认为"重新发送"
-	// 		},
-	// 		ajax: {							// 必选，对象，获取验证码的ajax设置
-	// 			url: "sendCode",					// 必选，字符串，获取验证码的请求地址
-	// 			// type: "",					// 可选，字符串，请求方式，默认为"POST"
-	// 			// dataType: "",				// 可选，字符串，返回的数据类型，默认为"json"
-	// 			// dataName: "",				// 可选，字符串，发送给服务器的数据对象中名值对的名，默认为填写手机号码或邮箱地址的input标签的name属性值
-	// 			// resultCodeName: "",			// 可选，字符串，返回数据的状态码名称，默认为"code"
-	// 			// resultMsgName: "",			// 可选，字符串，返回数据的消息名称，默认为"msg"
-	// 			successCode: "200"				// 可选，字符串或数值，返回数据的成功状态码，使用严格比较运算符进行比较，默认为1
-	// 		},
-	// 		alertBox: {						// 可选，对象，弹窗显示获取验证码的结果，不弹窗显示时可访问全局变量getCodeMsg以获取ajax返回信息
-	// 			className: "text",				// 可选，字符串，弹窗额外的类名
-	// 			close: true,				// 可选，布尔值，是否有关闭按钮，默认为false
-	// 			title: "孤岛提示",					// 可选，字符串，标题文本，默认无标题
-	// 			// buttons: [{					// 可选，数组，默认为一个“确定”按钮
-	// 			// 	value: "",				// 必选，字符串，按钮文本
-	// 			// 	callback: function() {}	// 可选，函数，点击按钮的回调函数，默认操作为关闭弹窗
-	// 			// }]
-	// 		}
-	// 	}
 	getCode({
 		form: {
 			targetType: "email",
@@ -133,7 +106,6 @@ $(function() {
 					}
 				}
 			});
-
 		}
 	});
 });
