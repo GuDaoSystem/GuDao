0056645f7bb027713ac1339ea59b3744e239fada
RainieWu <479086484@qq.com>
2018-04-30 23:04:57 +0800

    Picture模块

---
+++ GuDao/Admin/Controller/PictureController.class.php
@@ -0,0 +1,79 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+use Admin\Model\PictureModel;
+class PictureController extends Controller {
+    public function index(){
+        $this->display();
+    }
+
+
+    // 按乐队获取图片
+    public function getPictures() {
+    	$id = $_GET["id"];
+        $startIndex = ($_GET["pageIndex"] - 1) * $_GET["pageSize"];
+        $pageLength = $_GET["pageSize"];
+
+        $picture = new PictureModel();
+        $data = $picture->getPictures($id, $startIndex, $pageLength);
+
+        if($data) {
+            $result["code"] = 200;
+            $result["msg"] = "查询成功";
+            $result["data"] = $data;
+        } else {
+            $result["code"] = 201;
+            $result["msg"] = "查询失败";
+        }
+
+        $this->ajaxReturn($result);
+    }
+
+    // 删除图片
+    public function deletePicture() {
+    	$picture = new PictureModel();
+
+    	if($picture->deletePicture($_POST["id"])) {
+    		$result["code"] = 200;
+    		$result["msg"] = "删除成功";
+    	} else {
+    		$result["code"] = 201;
+    		$result["msg"] = "删除失败";
+    	}
+    	
+    	$this->ajaxReturn($result);
+    }
+
+    // 新增单张图片
+    public function addPicture() {
+    	$picture = new PictureModel();
+    	if($picture->addPicture($_POST["id"], $_POST["url"])) {
+    		$result["code"] = 200;
+    		$result["msg"] = "新增成功";
+    	} else {
+    		$result["code"] = 201;
+    		$result["msg"] = "新增失败";
+    	}
+    	$this->ajaxReturn($result);
+    }
+
+    // 新增多张图片
+    public function addPictures() {
+    	$id = $_POST["id"];
+    	$urls = $_POST["urls"];
+
+    	$picture = new PictureModel();
+    	for($i = 0; $i < count($urls); $i++) {
+    		if(!$picture->addPicture($id, $urls[$i])) {
+				$result["code"] = 201;
+    			$result["msg"] = "新增失败";
+    			$this->ajaxReturn($result);
+    		}
+    	}
+
+    	$result["code"] = 200;
+    	$result["msg"] = "新增成功";
+    	
+    	$this->ajaxReturn($result);
+    }
+}
--- GuDao/Admin/Model/PictureModel.class.php
+++ GuDao/Admin/Model/PictureModel.class.php
@@ -2,12 +2,29 @@
 namespace Admin\Model;
 use Think\Model;
 class PictureModel extends Model {
-	
-    // 按乐队获取图片
-    public function getPictureByBand($id) {
-        $picture = new PictureModel();
-        $map["band_id"] = $id;
-        $result = $picture->where($map)->getField("picture_url", true);
+
+	// 按乐队获取图片
+	public function getPictures($id, $startIndex, $pageLength) {
+		$picture = new PictureModel();
+		$map["band_id"] = $id;
+        $result = $picture->where($map)->limit($startIndex, $pageLength)->field("picture_id, picture_url")->select();
         return $result;
-    }
+	}
+
+	// 删除图片
+	public function deletePicture($id) {
+		$picture = new PictureModel();
+		$map["picture_id"] = $id;
+		$result = $picture->where($map)->delete();
+		return $result;
+	}
+
+	// 新增图片
+	public function addPicture($id, $url) {
+		$picture = new PictureModel();
+		$param["band_id"] = $id;
+		$param["picture_url"] = $url;
+		$result = $picture->add($param);
+		return $result;
+	}
 }
