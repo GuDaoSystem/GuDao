342cea71b973bf31acbcfb882b4cfed613bb180a
RainieWu <479086484@qq.com>
2018-05-02 20:55:51 +0800

    Band模块

--- GuDao/Admin/Controller/BandController.class.php
+++ GuDao/Admin/Controller/BandController.class.php
@@ -1,19 +1,126 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-use Admin\Model\AttendModel;
+// use Admin\Model\AttendModel;
 use Admin\Model\BandModel;
-use Admin\Model\CommentModel;
-use Admin\Model\PictureModel;
-use Admin\Model\ReplyModel;
-use Admin\Model\ShowModel;
-use Admin\Model\SupportModel;
-use Admin\Model\UserModel;
+// use Admin\Model\CommentModel;
+// use Admin\Model\PictureModel;
+// use Admin\Model\ReplyModel;
+// use Admin\Model\ShowModel;
+// use Admin\Model\SupportModel;
+// use Admin\Model\UserModel;
 class BandController extends Controller {
 	// 渲染页面
     public function index(){
     	$this->display();
     }
+
+
+    // 按页获取乐队
+    public function getBands() {
+        $startIndex = ($_GET["pageIndex"] - 1) * $_GET["pageSize"];
+        $pageLength = $_GET["pageSize"];
+
+        $band = new BandModel();
+        $data = $band->getBands($startIndex, $pageLength);
+        if($data) {
+            $result["code"] = 200;
+            $result["msg"] = "查询成功";
+            $result["data"] = $data;
+        } else {
+            $result["code"] = 201;
+            $result["msg"] = "查询失败";
+        }
+        $this->ajaxReturn($result);
+    }
+
+    // 删除乐队
+    public function deleteBand() {
+        $band = new BandModel();
+        if($band->deleteBand($_POST["id"])) {
+            $result["code"] = 200;
+            $result["msg"] = "删除成功";
+        } else {
+            $result["code"] = 201;
+            $result["msg"] = "删除失败";
+        }
+        $this->ajaxReturn($result);
+    }
+
+    // 新增乐队
+    public function addBand() {
+        $param["band_name"] = $_POST["name"];
+        $param["band_intro"] = $_POST["intro"];
+        if($_POST["cover"]) {
+            $param["band_cover"] = $_POST["cover"];
+        }
+        $param["band_initial"] = $_POST["initial"];
+
+        $band = new BandModel();
+        if($band->addBand($param)) {
+            $result["code"] = 200;
+            $result["msg"] = "新增成功";
+        } else {
+            $result["code"] = 201;
+            $result["msg"] = "新增失败";
+        }
+        $this->ajaxReturn($result);
+    }
+
+    // 修改乐队
+    public function modifyBand() {
+        $id = $_POST["id"];
+        if($_POST["name"]) {
+            $param["band_name"] = $_POST["name"];
+        }
+        if($_POST["intro"]) {
+            $param["band_intro"] = $_POST["intro"];
+        }
+        if($_POST["cover"]) {
+            $param["band_cover"] = $_POST["cover"];
+        }
+        if($_POST["initial"]) {
+            $param["band_initial"] = $_POST["initial"];
+        }
+
+        $band = new BandModel();
+        if($band->modifyBand($id, $param)) {
+            $result["code"] = 200;
+            $result["msg"] = "修改成功";
+        } else {
+            $result["code"] = 201;
+            $result["msg"] = "修改失败";
+        }
+        $this->ajaxReturn($result);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- GuDao/Admin/Model/BandModel.class.php
+++ GuDao/Admin/Model/BandModel.class.php
@@ -3,12 +3,34 @@ namespace Admin\Model;
 use Think\Model;
 class BandModel extends Model {
 
-    // 按ID获取乐队
-    public function getBand($id) {
+    // 按页获取乐队
+    public function getBands($startIndex, $pageLength) {
+        $band = new BandModel();
+        $result = $band->limit($startIndex, $pageLength)->select();
+        return $result;
+    }
+
+    // 删除乐队
+    public function deleteBand($id) {
         $band = new BandModel();
         $map["band_id"] = $id;
-        $result = $band->where($map)->select();
-        return $result[0];
+        $result = $band->where($map)->delete();
+        return $result;
+    }
+
+    // 新增乐队
+    public function addBand($param) {
+        $band = new BandModel();
+        $result = $band->add($param);
+        return $result;
+    }
+
+    // 修改乐队
+    public function modifyBand($id, $param) {
+        $band = new BandModel();
+        $map["band_id"] = $id;
+        $result = $band->where($map)->save($param);
+        return $result;
     }
 
 
