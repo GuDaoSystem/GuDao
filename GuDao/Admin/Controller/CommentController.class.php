0c344ed7b9740338517598f3090f8da8b9af3106
RainieWu <479086484@qq.com>
2018-05-02 20:36:23 +0800

    Comment模块

---
+++ GuDao/Admin/Controller/CommentController.class.php
@@ -0,0 +1,46 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+use Admin\Model\CommentModel;
+class CommentController extends Controller {
+    public function index(){
+        $this->display();
+    }
+
+
+    // 按页获取评论
+    public function getComments() {
+        $startIndex = ($_GET["pageIndex"] - 1) * $_GET["pageSize"];
+        $pageLength = $_GET["pageSize"];
+
+        $comment = new CommentModel();
+        $data = $comment->getComments($startIndex, $pageLength);
+
+        if($data) {
+            $result["code"] = 200;
+            $result["msg"] = "查询成功";
+            $result["data"] = $data;
+        } else {
+            $result["code"] = 201;
+            $result["msg"] = "查询失败";
+        }
+
+        $this->ajaxReturn($result);
+    }
+
+    // 删除评论
+    public function deleteComment() {
+        $comment = new CommentModel();
+        $data = $comment->deleteComment($_POST["id"]);
+
+        if($data) {
+            $result["code"] = 200;
+            $result["msg"] = "删除成功";
+        } else {
+            $result["code"] = 201;
+            $result["msg"] = "删除失败";
+        }
+
+        $this->ajaxReturn($result);
+    }
+}
--- GuDao/Admin/Model/CommentModel.class.php
+++ GuDao/Admin/Model/CommentModel.class.php
@@ -2,18 +2,19 @@
 namespace Admin\Model;
 use Think\Model;
 class CommentModel extends Model {
-	
-	// 按目标获取评论
-	public function getCommentByTarget($data) {
+
+	// 按页获取评论
+	public function getComments($startIndex, $pageLength) {
 		$comment = new CommentModel();
-		$result = $comment->where($data)->order("comment_time desc")->select();
-		return $result;
+        $result = $comment->order("comment_time desc")->limit($startIndex, $pageLength)->select();
+        return $result;
 	}
 
-	// 发送评论
-	public function sendComment($param) {
+	// 删除评论
+	public function deleteComment($id) {
 		$comment = new CommentModel();
-		$result = $comment->add($param);
+		$map["comment_id"] = $id;
+		$result = $comment->where($map)->delete();
 		return $result;
 	}
 }
